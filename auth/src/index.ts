8069e855fd1f68dbc11f978ef7fa454ff886cf57
mahaveer <mahaveersharma0496@yahoo.com>
2020-08-05 23:28:13 +0530

    Add not found handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const PORT = 3000
 const app = express()
@@ -19,6 +20,10 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
 
 app.use(errorHandler)
 
