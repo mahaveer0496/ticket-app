8eb92624477e45d886d8015b17a857b95cd2df42
mahaveer <mahaveersharma0496@yahoo.com>
2020-08-06 20:18:53 +0530

    Add current-user middleware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,32 @@
+import { RequestHandler } from 'express'
+import jwt from 'jsonwebtoken'
+
+interface UserPayload {
+  id: string
+  email: string
+}
+
+// telling typescript - find the Express.Request type defs and add currentUser property to it
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload
+    }
+  }
+}
+export const currentUser: RequestHandler = (req, res, next) => {
+  if (!req.session?.jwt) {
+    return next()
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY as string,
+    ) as UserPayload
+    req.currentUser = payload
+    res.send({ currentUser: payload })
+  } catch (error) {}
+
+  next()
+}
