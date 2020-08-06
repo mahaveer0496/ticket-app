bd60fc230d05f1e96bdbd45d122e9bc9c0cb0d4d
mahaveer <mahaveersharma0496@yahoo.com>
2020-08-06 16:22:52 +0530

    Add signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,10 @@
 import express, { Response, Request } from 'express'
+import jwt from 'jsonwebtoken'
 import { body } from 'express-validator'
 import { User } from '../models/user'
 import { validateRequest } from '../middlewares/validate-request'
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
 
 const router = express.Router()
 
@@ -15,6 +18,27 @@ router.get(
   async (req: Request, res: Response) => {
     const { email, password } = req.body
     const existingUser = await User.findOne({ email })
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password,
+    )
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY as string,
+    )
+    req.session = { jwt: userJwt }
+
+    res.status(200).send(existingUser)
   },
 )
 
