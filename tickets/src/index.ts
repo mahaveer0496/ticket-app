7827672683595313ce964179fac8043a9eb53546
mahaveer <mahaveersharma0496@yahoo.com>
2020-08-08 00:17:05 +0530

    Tickets microservice basic setup

---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+import { app } from './app'
+
+const PORT = 3000
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT not defined bruh')
+
+  try {
+    await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.error(error)
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+  })
+}
+
+start()
