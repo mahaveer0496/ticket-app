9d5cbcb64f657a2bd4a61c3cbc5f216628897837
mahaveer <mahaveersharma0496@yahoo.com>
2020-08-08 23:30:34 +0530

    Add test and route for showing ticket

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -6,6 +6,6 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
-
+  console.error(err)
   res.status(500).send({ errors: [{ message: 'Something went wrong' }] })
 }
--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,11 +1,30 @@
 import request from 'supertest'
 import { app } from '../../app'
 import { Ticket } from '../../models/ticket'
+import mongoose, { mongo } from 'mongoose'
+const route = (id = new mongoose.Types.ObjectId().toHexString()) =>
+  `/api/tickets/${id}`
 
-const route = '/api/tickets/:id'
-describe(`POST ${route}`, () => {
+describe(`GET /api/tickets/:id`, () => {
   it('should return 404 if ticket not found', async () => {
-    await request(app).get('/api/tickets/dowakdo').send().expect(404)
+    await request(app).get(route()).send().expect(404)
   })
-  it('should return ticket for valid id', async () => {})
+
+  it('should return ticket for valid id', async () => {
+    const payload = {
+      title: 'this is valid',
+      price: 10,
+    }
+    const response = await request(app)
+      .post('/api/tickets')
+      .set('Cookie', global.signin())
+      .send(payload)
+
+    const ticketResponse = await request(app)
+      .get(route(response.body.id))
+      .send()
+      .expect(200)
+
+    expect(ticketResponse.body).toMatchObject(payload)
+  })
 })
