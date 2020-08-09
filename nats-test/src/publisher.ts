288035e8bcaa12f67eb18c35929e33bca3602c21
mahaveer <mahaveersharma0496@yahoo.com>
2020-08-10 00:56:30 +0530

    Add type defs for publishers and listeners

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,17 +1,16 @@
 import nats from 'node-nats-streaming'
+import { TicketCreaterPublisher } from './events/ticket-created-publisher'
 console.clear()
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 })
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS')
-
-  const data = JSON.stringify({
-    id: 123,
+  const pub = new TicketCreaterPublisher(stan)
+  await pub.publish({
+    id: `123`,
     title: 'thie is title',
-  })
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published')
+    price: 20,
   })
 })
